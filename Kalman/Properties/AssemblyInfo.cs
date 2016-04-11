9de8420e7a72baa8ad40f3ed733dfee98f755470
zengfanlin1989 <243238340@qq.com>
2016-04-11 13:48:52 +0800

    更正 Signed-off-by: zengfanlin1989 <243238340@qq.com>

--- Kalman/Properties/AssemblyInfo.cs
+++ Kalman/Properties/AssemblyInfo.cs
@@ -2,35 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// 有关程序集的常规信息通过下列属性集
-// 控制。更改这些属性值可修改
+// 有关程序集的常规信息通过以下
+// 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Kalman")]
-[assembly: AssemblyDescription("Kalman开发框架")]
+[assembly: AssemblyTitle("Kalman123")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Kalman")]
-[assembly: AssemblyProduct("Kalman开发框架")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
-[assembly: AssemblyTrademark("Kalman")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Kalman123")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 使此程序集中的类型
-// 对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型，
-// 则将该类型上的 ComVisible 属性设置为 true。
+// 对 COM 组件不可见。  如果需要从 COM 访问此程序集中的类型，
+// 则将该类型上的 ComVisible 特性设置为 true。
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("ecc54f1f-919c-44a7-9ed8-1ef8c3fbef31")]
+[assembly: Guid("1f1ed67a-c095-4fd2-aa41-cfb4ecec3571")]
 
-// 程序集的版本信息由下面四个值组成:
+// 程序集的版本信息由下面四个值组成: 
 //
 //      主版本
 //      次版本 
-//      内部版本号
+//      生成号
 //      修订号
 //
-// 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
-// 方法是按如下所示使用“*”:
+// 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
+// 方法是按如下所示使用“*”: 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
